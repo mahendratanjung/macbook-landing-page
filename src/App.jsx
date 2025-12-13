06fee29a2c5fe049054aef4b8ecc730465f2d4ee
mahendratanjung <mahendratanjung100@gmail.com>
2025-12-14 03:32:23 +0800

    Implement 3D Product Viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import { ScrollTrigger} from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
+      <Showcase />
     </main>
   );
 };
