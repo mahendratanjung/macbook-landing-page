ecda9b7d2454b12d669cc18166eef2d623eb87bd
mahendratanjung <mahendratanjung100@gmail.com>
2025-12-15 23:53:16 +0800

    Implement Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from "gsap";
-import { ScrollTrigger} from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +18,8 @@ const App = () => {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+
     </main>
   );
 };
